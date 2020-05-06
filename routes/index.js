d49ffa6572a74a6b34a485180f06f3199c7ba3d4
Élon Castro <elon@ecgroup.com.br>
2020-05-06 17:15:12 -0300

    Entrega especial de rotas para as Controllers

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,32 @@ var express = require('express');
 var router = express.Router();
 const IndexController = require('../controllers/IndexController')
 
-router.get('/', IndexController.index)
 
+router.get('/login', LoginController.index);
+
+router.get('/inicio', InicioController.index);
+
+router.get('/avisos', AvisosController.index);
+
+router.get('/ocorrencias', OcorrenciasController.index);
+
+router.get('/reservas', ReservasController.index);
+
+router.get('/agenda', AgendaController.index);
+
+router.get('/enquetes', EnquetesController.index);
+
+router.get('/boleto', BoletoController.index);
+
+router.get('/contas', ContasController.index);
+
+router.get('/perdidos', perdidosController.index);
+
+router.get('/gerenciarPerfis', GerenciarPerfisController.index);
+
+router.get('/config', ConfigController.index);
+
+router.get('/perfil', PerfilController.index);
 
 
 module.exports = router;

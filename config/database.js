38828254ef5e7335396ccc3731acfbdf3f8c125e
ndebianchi <ndebianchi@outlook.com>
2020-05-06 17:00:09 -0300

    e é dada a largada

---
+++ config/database.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+module.exports = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql'
+}
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+const IndexController = require('../controllers/IndexController')
+
+router.get('/', IndexController.index)
+
+
+
+module.exports = router;

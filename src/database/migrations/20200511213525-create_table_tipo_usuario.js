b74bb867792582bfd632f285e8dbb0b284c1af13
muriloignaccio <murilohsignacio@gmail.com>
2020-05-11 20:50:33 -0300

    criando migração para gerar tabela usuario_tipos

---
+++ src/database/migrations/20200511213525-create_table_tipo_usuario.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('usuario_tipos', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      nome: {
+        type: Sequelize.STRING(32),
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+
+    return queryInterface.dropTable('usuario_tipos');
+
+  }
+};

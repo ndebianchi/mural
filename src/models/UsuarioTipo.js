fc5ac9276dccb65a1925e9a540c654576bd35bf4
muriloignaccio <murilohsignacio@gmail.com>
2020-05-12 01:09:27 -0300

    atualizar model de tipo de usuario

--- src/models/UsuarioTipo.js
+++ src/models/UsuarioTipo.js
@@ -3,5 +3,12 @@ module.exports = (sequelize, DataTypes) => {
     nome: DataTypes.STRING(32)
   });
 
+  UsuarioTipo.associate = models => {
+    UsuarioTipo.hasMany(models.Usuario, {
+      foreignKey: 'usuario_tipo_id',
+      as: 'usuarios'
+    })
+  };
+
   return UsuarioTipo;
 };

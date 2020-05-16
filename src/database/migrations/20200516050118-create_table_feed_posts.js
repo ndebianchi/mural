9abf37c1f89833a0e8228f6b146ee56c6a27da8e
muriloignaccio <murilohsignacio@gmail.com>
2020-05-16 02:32:01 -0300

    Criar migration da tabela feed_posts

---
+++ src/database/migrations/20200516050118-create_table_feed_posts.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('feed_posts', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      post_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'posts',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },      
+      foto: {
+        type: Sequelize.STRING(256)
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('feed_posts');
+  }
+};

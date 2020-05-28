fac6fcfa9894e95df8b749ae795c53490e89a70d
ndebianchi <ndebianchi@outlook.com>
2020-05-28 13:10:36 -0300

    C:/Program Files/Git/perfil com dados e posts do usuario

--- src/controllers/PerfilController.js
+++ src/controllers/PerfilController.js
@@ -1,8 +1,29 @@
+const { FeedPost, Post, Usuario } = require('../models');
+
 const PerfilController = {
-    index: (req, res) => {
+    index: async (req, res) => {       
+
+        const feeds = await FeedPost.findAll({
+            include: [{
+                    model: Post,
+                    as: "post",
+                    include: [
+                        "categoria",
+                        "usuario_visualizado",
+                        {
+                            model: Usuario,
+                            as: "usuario",
+                            include: ["apartamentos"]
+                        }
+                        
+                    ]
+                }]   
+        })      
+
         res.render('perfil', {
             pageTitle: 'Perfil',
-            usuario: req.session.usuario
+            usuario: req.session.usuario,
+            feeds
         })
     }
 }

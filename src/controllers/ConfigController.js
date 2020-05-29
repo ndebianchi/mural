729bc2223743b1f615e4bfa9320ae65999adb7e7
ndebianchi <ndebianchi@outlook.com>
2020-05-29 12:46:43 -0300

    atualizar dados usuario: concluido

--- src/controllers/ConfigController.js
+++ src/controllers/ConfigController.js
@@ -1,4 +1,4 @@
-const { }
+const { Usuario } = require('../models')
 const ConfigController = {
     index: (req, res) => {
         res.render('config', {
@@ -7,8 +7,34 @@ const ConfigController = {
             usuario: req.session.usuario
         })
     },
+
     update: (req, res) => {
+        // Desestrurar os dados que chegam
         let { nome, sobrenome, email, telefone } = req.body
+        
+        // Atualiza dados no DB
+        Usuario.update({
+            nome,
+            sobrenome,
+            email,
+            telefone
+        },{
+            where: { id: req.session.usuario.id }
+        })
+
+        // Atualiza dados da req.session.usuario:
+        if(nome != req.session.nome){req.session.usuario.nome = nome;};
+
+        if(sobrenome != req.session.sobrenome){req.session.usuario.sobrenome = sobrenome;};
+
+        if(email != req.session.email){req.session.usuario.email = email;};
+
+        if(telefone != req.session.telefone){req.session.usuario.telefone = telefone;}
+
+        // Feedback da Atualização (usar alert?)
+
+        // Finaliza Redirecionando de volta para Config
+        res.redirect('config');
                 
     }
 }

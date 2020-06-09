e4d440bd36c7bde89aaf8e527ec836089c855b83
muriloignaccio <murilohsignacio@gmail.com>
2020-06-09 03:13:51 -0300

    Importar middlewares na rota de cadastro

--- src/routes/cadastro.js
+++ src/routes/cadastro.js
@@ -1,11 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {
+  cadastroValidator,
+  erroValidator,
+} = require('../middlewares/validators');
 
 const CadastroController = require('../controllers/CadastroController');
 // const UsuarioLogado = require('../middlewares/UsuarioLogado');
 
 // router.get('/cadastro', UsuarioLogado, CadastroController.index);
 router.get('/cadastro', CadastroController.index);
-router.post('/cadastro', CadastroController.create)
+router.post(
+  '/cadastro',
+  cadastroValidator,
+  erroValidator,
+  CadastroController.store
+);
 
 module.exports = router;

dfcf5d44a1ea792fcdd594cc5a50f8f081e6a78c
muriloignaccio <murilohsignacio@gmail.com>
2020-06-09 03:15:46 -0300

    Reidentar sintaxes

--- src/controllers/AvisosController.js
+++ src/controllers/AvisosController.js
@@ -1,11 +1,9 @@
-const AvisosController = {
-    index: (req, res) => {
-        res.render('avisos', {
-            pageTitle: 'Avisos',
-            pageIcon: 'avisos.svg',
-            usuario: req.session.usuario
-        })
-    }
-}
-
-module.exports = AvisosController;
+module.exports = {
+  index: (req, res) => {
+    res.render('avisos', {
+      pageTitle: 'Avisos',
+      pageIcon: 'avisos.svg',
+      usuario: req.session.usuario,
+    });
+  },
+};
--- src/controllers/BoletoController.js
+++ src/controllers/BoletoController.js
@@ -1,11 +1,9 @@
-const BoletoController = {
-    index: (req, res) => {
-        res.render('boleto', {
-            pageTitle: '2ª Via do Condomínio',
-            pageIcon: '2via.svg',
-            usuario: req.session.usuario
-        })
-    }
-}
-
-module.exports = BoletoController;
+module.exports = {
+  index: (req, res) => {
+    return res.render('boleto', {
+      pageTitle: '2ª Via do Condomínio',
+      pageIcon: '2via.svg',
+      usuario: req.session.usuario,
+    });
+  },
+};

9f39a260bff0d633a6ce3b8156e7f5134e4846da
muriloignaccio <murilohsignacio@gmail.com>
2020-06-10 03:15:08 -0300

    Refatorar código

--- src/controllers/ContasController.js
+++ src/controllers/ContasController.js
@@ -1,18 +1,15 @@
-const ContasController = {
-    index: (req, res) => {
-        res.render('contas', {
-            pageTitle: 'Prestação de Contas',
-            pageIcon: 'contas2.svg',
-            usuario: req.session.usuario
-        })
-    },
-    editarGastos: (req, res) => {
-        res.render('editarGastos', {
-            pageTitle: 'Prestação de Contas',
-            pageIcon: 'contas2.svg'
-        })
-    }
-
-}
-
-module.exports = ContasController;
+module.exports = {
+  index: (req, res) => {
+    return res.render('contas', {
+      pageTitle: 'Prestação de Contas',
+      pageIcon: 'contas2.svg',
+      usuario: req.session.usuario,
+    });
+  },
+  editarGastos: (req, res) => {
+    return res.render('editarGastos', {
+      pageTitle: 'Prestação de Contas',
+      pageIcon: 'contas2.svg',
+    });
+  },
+};

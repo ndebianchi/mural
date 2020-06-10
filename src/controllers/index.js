28c12985d26fb6296c6cc54e2a394e0cc3c28989
muriloignaccio <murilohsignacio@gmail.com>
2020-06-10 03:10:34 -0300

    Otimizar importação de controllers

---
+++ src/controllers/index.js
@@ -0,0 +1,29 @@
+const AgendaController = require('./AgendaController');
+const AvisosController = require('./AvisosController');
+const BoletoController = require('./BoletoController');
+const CadastroController = require('./CadastroController');
+const ConfigController = require('./ConfigController');
+const ContasController = require('./ContasController');
+const GerenciarPerfisController = require('./GerenciarPerfisController');
+const InicioController = require('./InicioController');
+const LoginController = require('./LoginController');
+const OcorrenciasController = require('./OcorrenciasController');
+const PerdidosController = require('./PerdidosController');
+const PerfilController = require('./PerfilController');
+const ReservasController = require('./OcorrenciasController');
+
+module.exports = {
+  AgendaController,
+  AvisosController,
+  BoletoController,
+  CadastroController,
+  ConfigController,
+  ContasController,
+  GerenciarPerfisController,
+  InicioController,
+  LoginController,
+  OcorrenciasController,
+  PerdidosController,
+  PerfilController,
+  ReservasController,
+};

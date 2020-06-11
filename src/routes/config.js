0472fbcfc47f7d1ab1555facc8c9973545961eff
muriloignaccio <murilohsignacio@gmail.com>
2020-06-11 20:09:15 -0300

    Mudar método de atualização para put

--- src/routes/config.js
+++ src/routes/config.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
 
-const ConfigController = require('../controllers/ConfigController');
+const { ConfigController } = require('../controllers');
 
 router.get('/config', ConfigController.index);
-router.post('/atualizar-perfil', ConfigController.update);
+router.put('/config', ConfigController.update);
 
 module.exports = router;

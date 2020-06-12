d9358e8fe1159dd28219ee013a0b306e688638da
ndebianchi <ndebianchi@outlook.com>
2020-06-12 13:33:49 -0300

    email check

--- src/controllers/CadastroController.js
+++ src/controllers/CadastroController.js
@@ -15,32 +15,39 @@ module.exports = {
       foto = 'default.svg',
     } = req.body;
 
+    let emailCheck = await Usuario.findOne({ where: { email }})    
+
     try {
-      // Cria usuário a partir dos dados passados
-      const usuario = await Usuario.create({
-        nome,
-        sobrenome,
-        email,
-        senha: bcrypt.hashSync(senha, 10),
-        telefone,
-        foto,
-        usuario_tipo_id: 1,
-      });
+      // Checa se o usuario já existe por email, se existe lança o erro
+      if(emailCheck){
+        throw new ErroUsuarioExiste();
+      } else {
+          // Cria usuário a partir dos dados passados
+          const usuario = await Usuario.create({
+            nome,
+            sobrenome,
+            email,
+            senha: bcrypt.hashSync(senha, 10),
+            telefone,
+            foto,
+            usuario_tipo_id: 1,
+          });
 
-      // Checa se o apartamento já existe. Se não, cria um!
-      let [apto] = await Apartamento.findOrCreate({
-        where: { numero: apartamento, bloco },
-      });
+          // Checa se o apartamento já existe. Se não, cria um!
+          let [apto] = await Apartamento.findOrCreate({
+            where: { numero: apartamento, bloco },
+          });
 
-      //Adiciona apartamento ao usuário criado
-      usuario.addApartamentos(apto);
+          //Adiciona apartamento ao usuário criado
+          usuario.addApartamentos(apto);
 
-      // Redireciona para Login
-      return res.render('cadastro', {
-        sucesso: 'Usuário criado com sucesso!',
-      });
-    } catch (erro) {
-      return res.render('cadastro', {
+          // Redireciona para Login
+          return res.render('cadastro', {
+            sucesso: 'Usuário criado com sucesso!',
+          });      
+      }      
+    } catch (ErroUsuarioExiste) {
+        return res.render('cadastro', {
         erro: 'Usuário já existe',
       });
     }

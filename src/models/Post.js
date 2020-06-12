04235541a0b2717a0cbdbbbd5c7ece47778b0887
muriloignaccio <murilohsignacio@gmail.com>
2020-06-12 00:56:54 -0300

    Adicionar relacionamento da model Post necessária

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,33 +1,35 @@
 module.exports = (sequelize, DataTypes) => {
-    const Post = sequelize.define('Post', {
-      mensagem: DataTypes.STRING
-    });  
+  const Post = sequelize.define('Post', {
+    mensagem: DataTypes.STRING,
+  });
 
-    Post.associate = models => {
-      
-      Post.belongsTo(models.PostCategoria, {
-        foreignKey: 'categoria_id',
-        as: 'categoria'
-      });
+  Post.associate = (models) => {
+    Post.belongsTo(models.PostCategoria, {
+      foreignKey: 'categoria_id',
+      as: 'categoria',
+    });
 
-      Post.belongsTo(models.Usuario, {
-        foreignKey: 'usuario_id',
-        as: 'usuario'
-      });
+    Post.belongsTo(models.Usuario, {
+      foreignKey: 'usuario_id',
+      as: 'usuario',
+    });
 
-      Post.belongsToMany(models.Usuario, {
-        through: 'likes_vistos',
-        foreignKey: 'post_id',
-        as: 'usuario_visualizado'
+    Post.belongsToMany(models.Usuario, {
+      through: 'likes_vistos',
+      foreignKey: 'post_id',
+      as: 'usuario_visualizado',
     });
 
     Post.hasMany(models.PostPerdido, {
       foreignKey: 'post_id',
-      as: 'usuario_likes'
+      as: 'usuario_likes',
     });
-    
 
-    };
-  
-    return Post;
+    Post.hasMany(models.FeedPost, {
+      foreignKey: 'post_id',
+      as: 'post_feed',
+    });
   };
+
+  return Post;
+};

18b64b69aa4b94f9a339b0394fb36d6fc893cedc
ndebianchi <ndebianchi@outlook.com>
2020-06-18 16:24:18 -0300

    add filtro por data

--- src/controllers/AvisosController.js
+++ src/controllers/AvisosController.js
@@ -22,4 +22,36 @@ module.exports = {
       avisos: avisos.reverse(),
     });
   },
+  avisosFiltrados: async (req, res) => {
+    let { dataInicio, dataFinal } = req.body;   
+
+    const avisos = await Post.findAll({
+      where: { 
+        categoria_id: 1        
+       },
+      include: [
+        'categoria',
+        {
+          model: Usuario,
+          as: 'usuario',
+          include: ['apartamentos'],
+        },
+        'usuario_visualizado',
+      ],
+      attributes: {
+        include: ['created_at'],
+        exclude: ['categoria_id', 'usuario_id'],
+      },
+    }); 
+    
+    let avisosFiltrados = avisos.filter(a => {
+      return Date.parse(a.dataValues.created_at) >= Date.parse(dataInicio) &&
+             Date.parse(a.dataValues.created_at) <= Date.parse(dataFinal)            
+    })   
+    
+    return res.render('avisos', {
+      usuario: req.session.usuario,
+      avisos: avisosFiltrados.reverse(),
+    });
+  }
 };
--- src/routes/avisos.js
+++ src/routes/avisos.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const { AvisosController } = require('../controllers');
 
 router.get('/avisos', AvisosController.index);
+router.post('/avisosfiltro', AvisosController.avisosFiltrados);
 
 module.exports = router;

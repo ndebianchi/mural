d8560b936d10d132216bcbe4c975e312bcd0d410
muriloignaccio <murilohsignacio@gmail.com>
2020-06-21 22:54:36 -0300

    Atualizar localização da foto

--- src/controllers/CadastroController.js
+++ src/controllers/CadastroController.js
@@ -11,51 +11,47 @@ module.exports = {
       sobrenome,
       email,
       telefone,
-      senha      
+      senha,
     } = req.body;
-    
-    let foto = 'default.svg';
 
-    if(req.file){
-      foto = req.file.filename;
-    } 
+    const { location: foto = 'default.svg' } = req.file;
 
-    let emailCheck = await Usuario.findOne({ where: { email }})    
+    let emailCheck = await Usuario.findOne({ where: { email } });
 
     try {
       // Checa se o usuario já existe por email, se existe lança o erro
-      if(emailCheck){
+      if (emailCheck) {
         throw new ErroUsuarioExiste();
       } else {
-          // Cria usuário a partir dos dados passados
-          const usuario = await Usuario.create({
-            nome,
-            sobrenome,
-            email,
-            senha: bcrypt.hashSync(senha, 10),
-            telefone,
-            foto,
-            usuario_tipo_id: 1,
-          });
+        // Cria usuário a partir dos dados passados
+        const usuario = await Usuario.create({
+          nome,
+          sobrenome,
+          email,
+          senha: bcrypt.hashSync(senha, 10),
+          telefone,
+          foto,
+          usuario_tipo_id: 1,
+        });
 
-          // Checa se o apartamento já existe. Se não, cria um!
-          let [apto] = await Apartamento.findOrCreate({
-            where: { numero: apartamento, bloco },
-          });
+        // Checa se o apartamento já existe. Se não, cria um!
+        let [apto] = await Apartamento.findOrCreate({
+          where: { numero: apartamento, bloco },
+        });
 
-          //Adiciona apartamento ao usuário criado
-          usuario.addApartamentos(apto);
+        //Adiciona apartamento ao usuário criado
+        usuario.addApartamentos(apto);
 
-          // Redireciona para Login
-          return res.render('login', {
-            pageTitle: 'Mural | Login',
-            sucesso: 'Usuário criado com sucesso!'
-          });      
-      }      
+        // Redireciona para Login
+        return res.render('login', {
+          pageTitle: 'Mural | Login',
+          sucesso: 'Usuário criado com sucesso!',
+        });
+      }
     } catch (ErroUsuarioExiste) {
-        return res.render('cadastro', {
+      return res.render('cadastro', {
         erro: 'Usuário já existe',
       });
     }
-  }  
+  },
 };

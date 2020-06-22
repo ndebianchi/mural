6ac206a123abc821799181c0f70daa2f8e28bd0a
muriloignaccio <murilohsignacio@gmail.com>
2020-06-21 22:48:51 -0300

    Preparar rota para receber a imagem com o multer

--- src/routes/inicio.js
+++ src/routes/inicio.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
+const multerConfig = require('../config/multer');
+const multer = require('multer');
+const upload = multer(multerConfig);
+
 const { InicioController } = require('../controllers');
 
 router.get('/', InicioController.index);
 router.get('/inicio', InicioController.index);
-router.post('/novoPost', InicioController.novoPost);
+router.post('/novoPost', upload.single('foto'), InicioController.novoPost);
 router.post('/novaOcorrencia', InicioController.novaOcorrencia);
 router.post('/novoAviso', InicioController.novoAviso);
 router.post('/addLike/:id', InicioController.addLike);
